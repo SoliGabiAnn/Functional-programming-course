75affe883dcbbf289aa7246c138886de84d2a9cc
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-04-11 18:45:43 +0200

    Update 1_Lambda.py

--- Exercises/1_Lambda.py
+++ Exercises/1_Lambda.py
@@ -6,7 +6,7 @@ students = [
 ]
 
 # Fill out the lambda, so that this expression sorts by score descending
-# Hint: https://github.com/SoliGabiAnn/Functional-programming-course/wiki/3.-Anonymous-Functions-%E2%80%90-Lambda#lambda-in-sorted
+# Hint: https://github.com/SoliGabiAnn/Functional-programming-course/wiki/03.-Anonymous-Functions-%E2%80%90-Lambda#lambda-in-sorted
 sorted_students = sorted(students, key=lambda ..., reverse=True)
 
 for name, score in sorted_students:
