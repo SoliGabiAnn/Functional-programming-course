1e6546bd5525850c27fda5d540bc46b37f3f383b
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-04-11 18:13:19 +0200

    Create 2_Map.py

---
+++ Exercises/2_Map.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5]
+
+# Hint: https://github.com/SoliGabiAnn/Functional-programming-course/wiki/4.-Map-Vs-Starmap#why-use-it
+
+# Step 1: Square each number using map()
+squared_numbers = map( ... )
+
+# Step 2: Convert each squared number to a string using map()
+number_strings = map( ... )
+
+# Convert the map object to a list and print
+print(list(number_strings))
+
+
+'''
+Expected Output:
+
+['Number: 1', 'Number: 4', 'Number: 9', 'Number: 16', 'Number: 25']
+'''
